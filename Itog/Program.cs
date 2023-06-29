6324ed2a6a6d95b7f5339942ade6d9aca60ee35e
otp11 <otp1189@gmail.com>
2023-06-29 14:59:23 +0700

    code

--- Itog/Program.cs
+++ Itog/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿    static string[] FilterStrings(string[] strings)
+    {
+        int count = 0;
+        for (int i = 0; i < strings.Length; i++)
+        {
+            if (strings[i].Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        string[] result = new string[count];
+        int index = 0;
+        for (int i = 0; i < strings.Length; i++)
+        {
+            if (strings[i].Length <= 3)
+            {
+                result[index] = strings[i];
+                index++;
+            }
+        }
+
+        return result;
+    }
+        Console.WriteLine("Введите строки, разделенные пробелом:");
+        string input = Console.ReadLine();
+        string[] inputStrings = input.Split();
+        string[] filteredStrings = FilterStrings(inputStrings);
+        Console.WriteLine("Результат:");
+        foreach (string str in filteredStrings)
+        {
+            Console.WriteLine(str);
+        }
